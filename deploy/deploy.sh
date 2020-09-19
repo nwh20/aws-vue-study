6da80ca00b1df115cd3cf37bc4cc53d238e40a3a
nwh20 <hnamwon@gmail.com>
2020-09-19 16:33:13 +0900

    create deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,13 @@
+#!/bing/bash
+
+FR_DIR=/user/share/nginx/html
+REP_DIR=/home/ec2-user/app/step2/zip/dist
+
+echo "> FR_DIR File Del *"
+sudo rm -rf $FR_DIR/*
+
+echo "> File Copy to Frontend"
+sudo cp -r $REP_DIR/* $FR_DIR/
+
+echo "> Frontend Dir Confirm $FR_DIR"
+ls -l $FR_DIR
